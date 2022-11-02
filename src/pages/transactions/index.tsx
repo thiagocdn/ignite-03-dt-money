d7a15ed8481241170c4a598c4850aa9bd8242ef1
Thiago Cedran Santos <thiago.cedran@doji.co>
2022-11-02 20:49:39 -0300

    Created table

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,55 @@
 import { Header } from '../../components/header'
 import { Summary } from '../../components/summary'
+import {
+  PriceHightlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightlight variant="income">R$ 12.000,00</PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Hamburger</td>
+              <td>
+                <PriceHightlight variant="outcome">-R$ 60,00</PriceHightlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightlight variant="income">R$ 12.000,00</PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightlight variant="income">R$ 12.000,00</PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
