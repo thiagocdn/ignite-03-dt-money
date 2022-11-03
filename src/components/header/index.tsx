ee3bb1a9834f37fda325011dc44328ea09fe7a91
Thiago Cedran Santos <thiago.cedran@doji.co>
2022-11-02 21:30:43 -0300

    Created modal with portal and radix

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+
 import logoImg from '../../assets/logo.svg'
 
 export function Header() {
@@ -6,7 +8,20 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="DT Logo" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay>
+              <Dialog.Content>
+                <Dialog.Title>Nova Transação</Dialog.Title>
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
