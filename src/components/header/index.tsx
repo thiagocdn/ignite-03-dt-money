3e033bafaa62301208e96a617c3c4c865ef704c8
Thiago Cedran Santos <thiago.cedran@doji.co>
2022-11-02 21:47:16 -0300

    Created modal style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,25 +2,19 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../new-transaction-modal'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="DT Logo" />
+
         <Dialog.Root>
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova Transação</NewTransactionButton>
           </Dialog.Trigger>
-
-          <Dialog.Portal>
-            <Dialog.Overlay>
-              <Dialog.Content>
-                <Dialog.Title>Nova Transação</Dialog.Title>
-                <Dialog.Close />
-              </Dialog.Content>
-            </Dialog.Overlay>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
