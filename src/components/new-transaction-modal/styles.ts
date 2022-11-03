ef5f54771740cf112a7a4f2c16952b693bd8ca0c
Thiago Cedran Santos <thiago.cedran@doji.co>
2022-11-02 21:57:15 -0300

    Created income and outcome buttons

--- src/components/new-transaction-modal/styles.ts
+++ src/components/new-transaction-modal/styles.ts
@@ -69,3 +69,35 @@ export const CloseButton = styled(Dialog.Close)`
 
   color: ${(props) => props.theme['gray-500']};
 `
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome'
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  cursor: pointer;
+  background: ${(props) => props.theme['gray-700']};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  border: 0;
+  color: ${(props) => props.theme['gray-300']};
+
+  svg {
+    color: ${(props) => {
+      if (props.variant === 'income') return props.theme['green-300']
+      if (props.variant === 'outcome') return props.theme['red-300']
+      return props.theme['gray-300']
+    }};
+  }
+`
